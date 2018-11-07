18d2decd1e81d153ad022145f677b3a807f253fb
ConteAlexandre <TheLuffy@hotmail.fr>
2018-11-07 22:42:47 +0100

    connexion

--- MesVaccins.php
+++ MesVaccins.php
@@ -1,11 +1,12 @@
 <?php $title='Mes Vaccins'; ?>
 
 
-
+<?php include('inc/pdo.php') ?>
+<?php include('inc/fonction.php') ?>
 <?php include('inc/header.php'); ?>
 
 
-
+<?php print_r($_SESSION); ?>
 
 
 
