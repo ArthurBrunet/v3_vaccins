f47534608de150c500292a7abcc45f2748cec745
ConteAlexandre <TheLuffy@hotmail.fr>
2018-11-08 08:25:34 +0100

    rajout numero de lot

--- backoffice/newvaccins.php
+++ backoffice/newvaccins.php
@@ -13,14 +13,21 @@ $error=array();
         $contentvaccin=trim(strip_tags($_POST['content']));
         $error = veriftext($error,$contentvaccin,'content',3,1000);
 
+        $numerolot = trim(strip_tags($_POST['numerolot']));
+        $error = veriftext($error,$numerolot,'numerolot',3,8);
+
         $categorievac = $_POST['categorievac'];
 
+        $obligation = $_POST['obligation'];
+
         if (count($error)==0) {
-        $sql="INSERT INTO vaccins(vac_nom, vac_content, vac_categorie) VALUES ( :nom, :content, :categorievac)";
+        $sql="INSERT INTO vac_vaccins(nom, content, categorie, obligatoire, numerolot, created_at) VALUES ( :nom, :content, :categorievac, :obligation, :numerolot, NOW())";
         $query = $pdo->prepare($sql);
         $query->bindValue(':nom', $nomvaccin, PDO::PARAM_STR);
         $query->bindValue(':content', $contentvaccin, PDO::PARAM_STR);
+        $query->bindValue(':numerolot', $numerolot, PDO::PARAM_STR);
         $query->bindValue(':categorievac', $categorievac, PDO::PARAM_STR);
+        $query->bindValue(':obligation', $obligation, PDO::PARAM_STR);
         $query->execute();
         }
 
@@ -28,7 +35,6 @@ $error=array();
 
 
 ?>
-<!-- Requete pour inserer un nouveau vaccin dans sql -->
 
 
 
@@ -47,9 +53,18 @@ $error=array();
         <span> <?php if (!empty($error['content'])) { echo($error['content']); } ?></span>
         <br><input type="text" name="content" id="content" value="">
 
+        <br><label for="numerolot">Numero du lot: </label>
+        <span> <?php if (!empty($error['numerolot'])) { echo($error['numerolot']); } ?></span>
+        <br><input type="text" name="numerolot" id="numerolot" placeholder="G215468">
+        
         <br><label for="categorievac">Catégorie du vaccin: </label>
-        <br><input type="radio" name="categorievac" id="categorievac" value="Vivant"><label for="Vivant">Vivant</label>
-        <input type="radio" name="categorievac" id="categorievac" value="Inactive"><label for="Inactive">Inactive</label>
+        <br><input type="radio" name="categorievac" id="categorievac" value="vivant"><label for="Vivant">Vivant</label>
+        <input type="radio" name="categorievac" id="categorievac" value="inactive"><label for="Inactive">Inactive</label>
+
+        <br><label for="categorievac">Obligatoire ou non: </label>
+        <br><input type="radio" name="obligation" id="obligation" value="recommande"><label for="recommande">Recommandé</label>
+        <input type="radio" name="obligation" id="obligation" value="obligatoire"><label for="obligatoire">Obligatoire</label>
+
         <br><input type="submit" name="submittedvaccin" id="submittedvaccin" value="Envoyer">
 
 </form>
