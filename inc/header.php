b539d4b6910f64c90f2ce75fe941c6e57ccce32b
BaptisteBobot <bobot.baptiste@gmail.com>
2018-11-11 17:16:45 +0100

    14emecommit

--- inc/header.php
+++ inc/header.php
@@ -5,14 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title><?php $title ?></title>
-    <link rel="stylesheet" href="aset/style.css">
+    <link rel="stylesheet" href="asset/style.css">
 </head>
 <body>
     <div id="wraper">
     <header id="header">
       <a href="#" class="header-hamburger"></a>
         <div class="logo">
-            <img src="aset/images/logose.svg" alt="">
+            <img src="asset/images/logose.svg" alt="">
         </div>
         <div class="nav">
         <ul>
