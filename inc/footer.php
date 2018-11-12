8cd845f769a0a2b481ce41942b45561bcfac4490
BaptisteBobot <bobot.baptiste@gmail.com>
2018-11-12 12:54:56 +0100

    hdvfjgvg

--- inc/footer.php
+++ inc/footer.php
@@ -14,7 +14,13 @@
   src="https://code.jquery.com/jquery-3.3.1.min.js"
   integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
   crossorigin="anonymous"></script>
-<script src="asset/jquery.slicknav.min.js"></script>
+<script src="asset/jquery.slicknav.js"></script>
+
+<script>
+	$(function(){
+		$('#menu').slicknav();
+	});
+</script>
 
 
 </body>
